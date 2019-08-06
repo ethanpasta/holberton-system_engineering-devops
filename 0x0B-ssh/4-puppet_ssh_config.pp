1154b1ea0b6c9d13a5b999bc00f0444faa2dfff2
ethanpasta <ethan.mayer9@gmail.com>
2019-08-06 18:43:31 +0000

    Add stdlib package

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Manifest adds 2 lines to ssh configuration file
+include stdlib
+
 file_line { 'no_password':
   path   => '/etc/ssh/ssh_config',
-  ensure => 'present',
   line   => '    PubkeyAuthentication no',
 }
 
 file_line { 'identity_file':
   path   => '/etc/ssh/ssh_config',
-  ensure => 'present',
   line   => '    IdentityFile ~/.ssh/holberton',
 }
