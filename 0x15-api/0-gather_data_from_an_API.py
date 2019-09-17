e49ee24110c663a6a7c66abee2a147f37a010c5f
ethanpasta <ethan.mayer9@gmail.com>
2019-09-17 01:20:59 +0000

    Add __name__ == __main__

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ Module for task 0 """
-import requests
-from sys import argv
 
-user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                    .format(argv[1])).json()
-todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                    .format(argv[1])).json()
-todo_complete = [todo_m for todo_m in todo if todo_m.get("completed")]
-print("Employee {} is done with tasks({}/{})".format(user.get("name"),
-                                                     len(todo_complete),
-                                                     len(todo)))
-for task in todo_complete:
-    print('\t{}'.format(task.get("title")))
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(argv[1])).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(argv[1])).json()
+    todo_complete = [todo_m for todo_m in todo if todo_m.get("completed")]
+    print("Employee {} is done with tasks({}/{})".format(user.get("name"),
+                                                         len(todo_complete),
+                                                         len(todo)))
+    for task in todo_complete:
+        print('\t{}'.format(task.get("title")))
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """ Module for task 3 """
-import json
-import requests
 
-users = requests.get("https://jsonplaceholder.typicode.com/users").json()
-json_dic = {}
-for user in users:
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                        .format(user.get("id"))).json()
-    json_dic["{}".format(user.get("id"))] = [
-        {"task": task.get("title"),
-         "completed": task.get("completed"),
-         "username": user.get("username")} for task in todo]
-with open('todo_all_employees.json'.format(user.get("id")), mode='w') as f:
-    json.dump(json_dic, f)
+if __name__ == "__main__":
+    import json
+    import requests
+
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    json_dic = {}
+    for user in users:
+        todo = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId={}"
+            .format(user.get("id"))).json()
+        json_dic["{}".format(user.get("id"))] = [
+            {"task": task.get("title"),
+             "completed": task.get("completed"),
+             "username": user.get("username")} for task in todo]
+        with open('todo_all_employees.json'.format(
+                user.get("id")), mode='w') as f:
+            json.dump(json_dic, f)
