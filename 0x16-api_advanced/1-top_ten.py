ba935a6060dc220d0fbc102b08231a14d0bffc3b
ethanpasta <ethan.mayer9@gmail.com>
2019-09-19 19:17:05 +0000

    Change import to inside of function

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """ Module for task 1 """
-import requests
 
 
 def top_ten(subreddit):
+    import requests
+
     headers = {
-        'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                       'AppleWebKit/537.36 (KHTML, like Gecko) '
-                       'Chrome/76.0.3809.132 Safari/537.36')
+        'User-Agent': 'My-User-Agent'
     }
     url = "https://www.reddit.com/r/{}/top.json?limit=10".format(subreddit)
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code > 300:
         print("None")
-        return
-    listings = r.json()["data"]["children"]
-    for listing in listings:
-        print(listing["data"]["title"])
+    else:
+        [print(d.get("data").get("title"))
+         for d in r.json().get("data").get("children")]
