5416f7c988c89dd8f0e94aa936b98119e299f604
ethanpasta <ethan.mayer9@gmail.com>
2019-10-02 20:16:32 +0000

    Add /bin to path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# puppet fixes apache2 server
+# puppet manifest fixes wordpress website
 exec { 'Replace line in wp-settings.php':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/usr/bin', '/usr/sbin',],
+  path    => ['/usr/bin', '/usr/sbin', '/bin',],
 }
